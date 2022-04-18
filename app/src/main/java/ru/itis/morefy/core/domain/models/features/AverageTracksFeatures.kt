1086114c6f67063e6c3a3472b41c88dcd42101e1
renett_t <rtapkina@gmail.com>
2022-04-18 10:13:42 +0300

    spoti entities created

---
+++ app/src/main/java/ru/itis/morefy/core/domain/models/features/AverageTracksFeatures.kt
@@ -0,0 +1,20 @@
+package ru.itis.morefy.core.domain.models.features
+
+data class AverageTracksFeatures (
+    private val source: Set<TrackFeatures>?,
+
+    private val acousticness: Float,
+    private val danceability: Float,
+    private val energy: Float,
+    private val instrumentalness: Float,
+    private val liveness: Float,
+    private val loudness: Float,
+    private val speechiness: Float,
+    private val valence: Float,
+
+    private val mode: Int,
+    private val key: Int,
+
+    private val tempo: Float,
+    private val timeSignature: Int,
+)
