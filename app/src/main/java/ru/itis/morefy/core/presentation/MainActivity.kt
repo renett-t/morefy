6fc6267275a4d17f013d86f97f8d357c497c98c5
Oleg S <olegsolovyanenko@gmail.com>
2022-04-22 01:03:51 +0300

    NavAndToolbarAdded
    Started work with nav and Tool bars,
    but this does not apply to databases
    Fixed bug with gradle.properties

--- app/src/main/java/ru/itis/morefy/core/presentation/MainActivity.kt
+++ app/src/main/java/ru/itis/morefy/core/presentation/MainActivity.kt
@@ -2,11 +2,37 @@ package ru.itis.morefy.core.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.itis.morefy.R
+import ru.itis.morefy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var controller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        controller = findController(R.id.container)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(controller)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onBackPressed() {
+        when(supportFragmentManager.backStackEntryCount){
+            0 -> super.onBackPressed()
+            else -> supportFragmentManager.popBackStack()
+        }
+    }
+
+    fun AppCompatActivity.findController (id: Int) : NavController {
+        return (supportFragmentManager.findFragmentById(id) as NavHostFragment).navController
     }
 }
