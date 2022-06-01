31e1f75cf0efd2fe449f195c98b9e16f8bf54a8b
renett_t <rtapkina@gmail.com>
2022-06-01 20:47:17 +0300

    track fragment layout

---
+++ app/src/main/java/ru/itis/morefy/core/presentation/fragments/details/TrackFragment.kt
@@ -0,0 +1,15 @@
+package ru.itis.morefy.core.presentation.fragments.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import ru.itis.morefy.R
+
+
+class TrackFragment : Fragment(R.layout.fragment_track) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
