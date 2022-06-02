1342b16b4183ea835202355c5213231673ef1138
BigWickedDaddy <81323621+BigWickedDaddy@users.noreply.github.com>
2022-06-02 19:07:51 +0300

    Support is done

--- app/src/main/java/ru/itis/morefy/core/presentation/fragments/SupportFragment.kt
+++ app/src/main/java/ru/itis/morefy/core/presentation/fragments/SupportFragment.kt
@@ -3,21 +3,39 @@ package ru.itis.morefy.core.presentation.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import ru.itis.morefy.R
+import ru.itis.morefy.databinding.FragmentSupportBinding
 
 
 class SupportFragment : Fragment(R.layout.fragment_support) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding: FragmentSupportBinding
+
+    private var strMsg: String = ""
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSupportBinding.bind(view)
+
+        with(binding){
+            btnSend.setOnClickListener {
+                strMsg = txtMsg.text.toString()
+                composeEmail(strMsg)
+            }
+        }
     }
 
-    fun composeEmail(addresses: Array<String?>?, subject: String?) {
-        val intent = Intent(Intent.ACTION_SENDTO)
-        intent.data = Uri.parse("mailto:") // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, addresses)
-        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    fun composeEmail(strMsg: String) {
+        val adresses = arrayOf("vzlommamonta@gmail.com")
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.data = Uri.parse("mailto:")
+        emailIntent.type = "text/plain"
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, adresses)
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Support Morefy App")
+        emailIntent.putExtra(Intent.EXTRA_TEXT, strMsg)
 
-        startActivity(intent)
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."))
+        }
     }
-}
+
